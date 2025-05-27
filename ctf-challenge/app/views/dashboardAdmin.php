00d89a02c83da00b0e3d56371983b66fdd8a1846
Anna De Matos <anna@MacBook-Air-de-Anna.local>
2025-05-27 11:34:47 +0200

    Ajout nav bar dashboard admin

--- ctf-challenge/app/views/dashboardAdmin.php
+++ ctf-challenge/app/views/dashboardAdmin.php
@@ -1,5 +1,5 @@
 <?php include __DIR__ . '/includes/admin/dashboard/headerDashboard.php'; ?>
-<?php include __DIR__ . '/includes/admin/navAdmin.php'; ?>
+<?php include __DIR__ . '/includes/admin/dashboard/navAdmin.php'; ?>
 
 <main>
 
---
+++ ctf-challenge/app/views/includes/admin/dashboard/navAdmin.php
@@ -0,0 +1,22 @@
+ <nav>
+    <div id="nav-logo">
+        <img src="/ctf_anna/ctf-challenge/public/images/LCT-03.png" alt="Logo LCT">
+    </div>
+    <ul id="nav-tabs">
+        <li class="nav-item">
+            <a id="nav-home" href="/admin/home">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a id="nav-teams" href="/admin/teams">Équipes</a>
+        </li>
+        <li class="nav-item">
+            <a id="nav-players" href="/admin/players">Joueurs</a>
+        </li>
+        <li class="nav-item">
+            <a id="nav-challenges" href="/admin/challenges">Challenges</a>
+        </li>
+        <li class="nav-item">
+            <a id="nav-config" href="/admin/config">Configuration</a>
+        </li>
+    </ul>
+</nav>
