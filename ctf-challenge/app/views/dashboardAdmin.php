8da0f7e685290343fd6df41fa8dc741ec695a3b4
Anna De Matos <anna@MacBook-Air-de-Anna.local>
2025-05-27 15:43:59 +0200

    Ajout page d'accueil dashboard admin

--- ctf-challenge/app/views/dashboardAdmin.php
+++ ctf-challenge/app/views/dashboardAdmin.php
@@ -2,7 +2,7 @@
 <?php include __DIR__ . '/includes/admin/dashboard/navAdmin.php'; ?>
 
 <main>
-
+<?php include __DIR__ . '/includes/admin/dashboard/homeAdmin.php'; ?>
 </main>
 
 </body>
---
+++ ctf-challenge/app/views/includes/admin/dashboard/homeAdmin.php
@@ -0,0 +1,82 @@
+<div id="admin-dashboard">
+    <div id="home-title">
+        <h2>Panneau d'administration</h2>
+    </div>
+    <div id="card-scoreboard-admin">
+        <div id="card-scoreboard-admin-content">
+            <h3>Tableau des scores par équipe</h3>
+            <div id="scoreboard-admin">
+                <table>
+            <thead>
+                <tr>
+                    <th>Rang</th>
+                    <th>Équipe</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+                <tr>
+                    <td>7</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+                <tr>
+                    <td>8</td>
+                    <td>Équipe 1</td>
+                    <td>🟢</td>
+                </tr>
+            </tbody>
+        </table>
+            </div>
+        </div>
+    </div>
+    <div id="card-statistics">
+        <div id="statistics-title">
+            <h3>Statistiques</h3>
+        </div>
+        <div id="statistics-teams">
+            <h5>Nombre d'équipes inscrites :</h5>
+            <h3>8</h3>
+        </div>
+        <div id="statistics-flags">
+            <h5>Total de flags trouvés :</h5>
+            <h3>43</h3>
+        </div>
+    </div>
+    <div id="card-logout">
+        <div id="logout-btn">
+            <h5>Déconnexion</h5>
+            <button>Déconnexion</button>
+        </div>
+    </div>
+</div>
